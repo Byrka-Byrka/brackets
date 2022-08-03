3bf488f41e3f51f0740408a11125460a2c46a87c
Byrka-Byrka <75577174+Byrka-Byrka@users.noreply.github.com>
2022-08-03 16:00:46 +0300

    feat: add bracket function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let strArr = str.split('')
+  let openBracket = []
+  let closedBracket = []
+  let closedIndex = 0
+  let openIndex = 0
+  bracketsConfig.forEach(e=>{
+    openBracket.push(e[0])
+    closedBracket.push(e[1])
+  })
+  let stack = []
+
+  for(let i = 0; i < strArr.length; i++) {
+    openIndex = openBracket.indexOf(strArr[i])
+    closedIndex = closedBracket.indexOf(strArr[i])
+    if(openIndex === closedIndex && stack[stack.length-1] === openIndex){
+      stack.pop()
+      continue;
+    }
+    if (openIndex !== -1) {
+      stack.push(openIndex)
+    } else if (closedIndex !== -1){
+      openIndex = stack.pop()
+      if (closedIndex !== openIndex) return false
+    }
+  }
+  if(stack.length > 0) return false
+  return true
 }
